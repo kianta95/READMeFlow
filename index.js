9f54028dea1b16b43f266d91a2b8c335dc0af32e
Kianta McDuffie <mcduffie.kianta@gmail.com>
2024-05-05 16:45:34 -0400

    Correct files

---
+++ index.js
@@ -0,0 +1,47 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your application? Provide installation instructions:'
+    },
+];
+    
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`README.md successfully generated as ${fileName}`);
+    });
+}
+
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            const markdown = generateMarkdown(answers);
+            writeToFile('README.md', markdown);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,58 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions about this project, please contact [${data.kianta95}](https://github.com/${data.kianta95}) at ${data.mcduffie.kianta@gmail.com}
+`;
+}
+
+module.exports = generateMarkdown;
